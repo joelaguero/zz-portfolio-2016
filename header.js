3bd31294cff18d33518f4a4587b36de7f54cbb2c
joelmaguero <ja5732@gmail.com>
2014-03-29 11:21:24 -0700

    Added Google analytics

--- header.js
+++ header.js
@@ -29,3 +29,11 @@ $('#card3').click(function() {
 $('#card4').click(function() {
 	window.location = 'motion.html';
 });
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-46399402-1', 'joelaguero.com');
+ga('send', 'pageview');
