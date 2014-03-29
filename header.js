f2cc0b844092a7b0ba03303f39df3885f02559d5
joelmaguero <ja5732@gmail.com>
2014-03-28 22:54:21 -0700

    Initial commit

---
+++ header.js
@@ -0,0 +1,31 @@
+$('#home-button').click(function() {
+	window.location = 'index.html';
+});
+
+$('#resume').click(function() {
+	window.location = 'joel-aguero-resume-0214.pdf';
+});
+
+$('#email').click(function() {
+	window.location = 'mailto:hello@joelaguero.com';
+});
+
+$('#twitter').click(function() {
+	window.location = 'http://www.twitter.com/joelaguero';
+});
+
+$('#card1').click(function() {
+	window.location = 'sproose.html';
+});
+
+$('#card2').click(function() {
+	window.location = 'gogroup.html';
+});
+
+$('#card3').click(function() {
+	window.location = 'yelp.html';
+});
+
+$('#card4').click(function() {
+	window.location = 'motion.html';
+});
