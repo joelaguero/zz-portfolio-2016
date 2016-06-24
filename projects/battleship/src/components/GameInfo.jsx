e36452c5250163a0291ff30f7f41c1cf535518d6
Joel Aguero <joelmaguero@gmail.com>
2016-06-24 15:38:04 -0700

    Add line explaining player colors

--- projects/battleship/src/components/GameInfo.jsx
+++ projects/battleship/src/components/GameInfo.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 const GameInfo = (props) => (
   <div id='game-info'>
     <h1>Welcome to Battleship</h1>
+    <p>Player 1 is blue. Player 2 is red.</p>
     <div>
       <strong>Who's turn?</strong>
       <p id="player-turn">{ props.playerTurn }</p>
